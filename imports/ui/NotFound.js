d7063ed216b45c3a11a08ec8982beb76f309b4f2
Roger Montserrat <ruchy.mr@gmail.com>
2017-09-06 11:15:55 +0200

    Modifying 404 not found

--- imports/ui/NotFound.js
+++ imports/ui/NotFound.js
@@ -5,7 +5,7 @@ export default () => {
   return (
       <div className="boxed-view">
         <div className="boxed-view__box">
-          <h1>Page not found</h1>
+          <h1>404 - Page not found</h1>
           <p>Mmm, nothing to see here... </p>
           <Link className="button button--link" to="/">Go Home!</Link>
         </div>
